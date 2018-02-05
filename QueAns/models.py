c0e51ad26063cfe4bf9e778c354888ac20f0c0cd
prabal1815 <prabalr@squadrun.co>
2018-02-05 18:40:51 +0530

    Fixed models.py and other bugs in second review

--- QueAns/models.py
+++ QueAns/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User,Permission
-from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 class Meetings(models.Model):
     HOMER = 1
