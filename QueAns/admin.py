d42de753e5f7c6ef99e188d7cac9251b35b0ae04
prabal1815 <prabalr@squadrun.co>
2018-02-07 01:01:28 +0530

    Fixed the Meetings and Questions table

--- QueAns/admin.py
+++ QueAns/admin.py
@@ -1,43 +1,76 @@
 from django.contrib import admin
-from .models import Question,Meetings,Message,Voting,Satisfaction
+from .models import Question, Meetings, Message, Voting, Satisfaction
+# from datetime import date
 
 
 class MeetingsAdmin(admin.ModelAdmin):
-    def has_change_permission(self, request, obj=None):
-        return (request.method in ['GET', 'HEAD'] and
-                super().has_change_permission(request, obj))
+    list_display = ['meetings_text']
+
     def get_readonly_fields(self, request, obj=None):
-        return ['id','venue','moderator','start_time','end_time']
+        if request.user == obj.moderator:
+            return ['moderator']
+        else:
+            return ['id','meetings_text', 'venue', 'moderator', 'start_time', 'end_time']
+
     def has_add_permission(self, request):
         if request.user.is_superuser:
             return True
+
     def has_delete_permission(self, request, obj=None):
         return False
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['question_text','anon_status','meeting_id',]
-    list_filter = ['meeting_id',]
+    fields = ['question_text', 'anon_status', 'meeting_id','asker']
+    list_filter = ['meeting_id', ]
+    list_display = ['question_text']
+
+    def get_fieldsets(self, request, obj=None):
+        if obj is not None:
+            if obj.anon_status == 1:
+                return [(None, {'fields': ('question_text','meeting_id','anon_status',)})]
+
+        return [(None, {'fields': ('question_text', 'anon_status', 'meeting_id','asker')})]
+
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            if not request.user == obj.asker:
+                return ['question_text', 'anon_status', 'meeting_id','asker']
+            else:
+                return ['asker']
+        return self.readonly_fields
 
     def save_model(self, request, obj, form, change):
+        """
+        List of parameters of save_model:
+        :param request: to access the current user
+        :param obj: to access the attributes of the model
+        :param form: --
+        :param change: --
+        :return: super(function)
+        """
         obj.asker = request.user
-        obj.save()
+        return super(QuestionAdmin,self).save_model(request, obj, form, change)
+
     def has_delete_permission(self, request, obj=None):
         return False
-""" 
-Working here--- IGNORE THE COMMENT
+
+
 class VotingAdmin(admin.ModelAdmin):
-    list_display = ['user_id','question_id','vote',]
-    fields = ['vote',]
+    list_display = ['user_id', 'question_id', 'vote', ]
+    fields = ['vote', ]
+
     def save_model(self, request, obj, form, change):
         obj.user_id = request.user
         obj.save()
+
     def has_delete_permission(self, request, obj=None):
         return False
-"""
-admin.site.register(Question,QuestionAdmin)
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Meetings, MeetingsAdmin)
 admin.site.register(Message)
-admin.site.register(Voting)
+admin.site.register(Voting, VotingAdmin)
 admin.site.register(Satisfaction)
-
--- QueAns/models.py
+++ QueAns/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Meetings(models.Model):
     HOMER = 1
     PLATO = 2
     JARVIS = 3
     FRIDAY = 4
     VENUE_CHOICES = (
-        (HOMER,'homer'),
-        (PLATO,'plato'),
-        (JARVIS,'jarvis'),
-        (FRIDAY,'friday'),
+        (HOMER, 'homer'),
+        (PLATO, 'plato'),
+        (JARVIS, 'jarvis'),
+        (FRIDAY, 'friday'),
     )
-    venue = models.SmallIntegerField(choices=VENUE_CHOICES,default=HOMER)
+
+    meetings_text = models.CharField(max_length=100)
+    venue = models.SmallIntegerField(choices=VENUE_CHOICES, default=HOMER)
     moderator = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.DateTimeField(help_text="Start time of the meeting")
     end_time = models.DateTimeField(help_text="End time of the meeting")
+
+    def __str__(self):
+        return self.meetings_text
+
 
 class Question(models.Model):
     YES = 1
@@ -24,20 +31,17 @@ class Question(models.Model):
         (YES, 'yes'),
         (NO, 'no'),
     )
-    question_text = models.CharField(max_length=300)
-    meeting_id = models.ForeignKey(Meetings,on_delete=models.CASCADE)
-    asker = models.ForeignKey(User,on_delete=models.CASCADE)
-    anon_status = models.SmallIntegerField(choices=ANON_CHOICES,default=NO)
 
-    def __str__(self):
-        if self.anon_status == 1:
-            return "%s MEETING ID: %s AUTHOR:ANONYMOUS" % (self.question_text,self.meeting_id)
-        else:
-            return '%s MEETING ID: %s  AUTHOR: %s' % (self.question_text,self.meeting_id,self.asker)
+    question_text = models.CharField(max_length=300)
+    meeting_id = models.ForeignKey(Meetings, on_delete=models.CASCADE)
+    asker = models.ForeignKey(User, on_delete=models.CASCADE)
+    anon_status = models.SmallIntegerField(choices=ANON_CHOICES, default=NO)
+
 
 class Message(models.Model):
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    text = models.CharField(max_length = 1000)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+
 
 class Voting(models.Model):
     UPVOTE = 1
@@ -48,9 +52,11 @@ class Voting(models.Model):
         (DOWNVOTE, 'Downvote'),
         (NONE, 'None'),
     )
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
-    vote = models.SmallIntegerField(choices=VOTING_CHOICES,default=NONE)
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    vote = models.SmallIntegerField(choices=VOTING_CHOICES, default=NONE)
+
 
 class Satisfaction(models.Model):
     YES = 1
@@ -59,6 +65,7 @@ class Satisfaction(models.Model):
         (YES, 'yes'),
         (NO, 'no'),
     )
+
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    satisf_status = models.BooleanField(choices=SATISFACTION_CHOICES,default=YES)
+    satisf_status = models.BooleanField(choices=SATISFACTION_CHOICES, default=YES)
